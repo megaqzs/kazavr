041c6a79cdecf602a58214e85240ffab703b8ac9
megaqzs <91798921+megaqzs@users.noreply.github.com>
2025-11-27 00:53:16 +0200

    use sleep mode to reduce power consumption

--- main/main.c
+++ main/main.c
@@ -3,23 +3,52 @@
 #include <avr/interrupt.h>
 #include <uart.h>
 #include "matrix.h"
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+#include <avr/power.h>
 #include <wire.h>
 #include <Arduino.h>
 
-void set_clock_prescaler_div2() {
-    CLKPR = 0x80;
-    CLKPR = 1;
-}
+// watchdog interrupt
+ISR (WDT_vect) 
+{
+   wdt_disable();  // disable watchdog
+}  // end of WDT_vect
 
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
-    set_clock_prescaler_div2();
+    clock_prescale_set(clock_div_2);
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
     setupPins();
     uart_init();
-
 }
 void loop()
 {
-    scan_loop();
+  scan_loop();
+  delayMicroseconds(200);
+  // disable ADC
+  ADCSRA = 0;  
+
+  // clear various "reset" flags
+  MCUSR = 0;     
+  // allow changes, disable reset
+  WDTCSR = bit (WDCE) | bit (WDE);
+  // set interrupt mode and an interval 
+  WDTCSR = bit (WDIE);    // set WDIE, and 16ms seconds delay
+  wdt_reset();  // pat the dog
+  
+  set_sleep_mode (SLEEP_MODE_PWR_DOWN);  
+  noInterrupts ();           // timed sequence follows
+  sleep_enable();
+ 
+  // turn off brown-out enable in software
+  MCUCR = bit (BODS) | bit (BODSE);
+  MCUCR = bit (BODS); 
+  interrupts ();             // guarantees next instruction executed
+  sleep_cpu ();  
+  
+  // cancel sleep as a precaution
+  sleep_disable();
 }
 
